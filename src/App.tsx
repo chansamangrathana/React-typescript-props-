3260f5cdda0a8cc9dd9b1ebbc23e0ff01b261b7a
chansamangrathana <chansamangrathana1607@gmail.com>
2024-03-15 12:02:02 -0700

    component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { Button } from "flowbite-react";
+import CardComponent from "./componenet/CardComponent";
 
-
-
+type Status = "idle" | "loading" | "success" | "error";
+type Product = {
+  readonly id: 1;
+  title: "...";
+  price: "...";
+  category: "...";
+  description: "...";
+  image: "...";
+};
 
 function App() {
-  const [count, setCount]=useState(0)
-
-  useEffect(()=>{
-
-  })
+  const [products, setProducts] = useState<Product[]>([]);
+  const [status, setStatus] = useState<Status>("idle");
+  useEffect(() => {
+    setStatus("loading");
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setStatus("success");
+      })
+      .catch((err) => {
+        setStatus("error");
+      });
+  }, []);
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    
       
+      <div className=" mx-16 grid grid-flow-row grid-cols-4 gap-4">
+        {products.map((product) => (
+          <CardComponent
+            key={product.id}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+          />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
