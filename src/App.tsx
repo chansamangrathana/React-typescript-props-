2de3bae24248025e0e074dfa5a0a8472e814e51e
chansamangrathana <chansamangrathana1607@gmail.com>
2024-03-14 23:20:33 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+
+
+
+function App() {
+  const [count, setCount]=useState(0)
+
+  useEffect(()=>{
+
+  })
+
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    
+      
+    </>
+  )
+}
+
+export default App
